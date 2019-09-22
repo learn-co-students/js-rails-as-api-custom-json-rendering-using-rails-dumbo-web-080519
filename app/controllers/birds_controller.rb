45e5824a167b6a5b53a9e753ffa9709a5af44ee4
Brian Wong <bwong92@gmail.com>
2019-09-22 15:30:24 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,7 +1,8 @@
 class BirdsController < ApplicationController
   def index
     birds = Bird.all
-    render json: birds
+    # render json: birds, only: [:id, :name, :species]
+    render json: birds, except: [:created_at, :updated_at]
   end
 
   def show
@@ -9,7 +10,8 @@ class BirdsController < ApplicationController
     # render json: {id: bird.id, name: bird.name, species: bird.species}
     # What thisç
     if bird 
-      render json: {id: bird.id, name: bird.name, species: bird.species}
+      render json: bird.slice(:id, :name, :species)
+      # render json: {id: bird.id, name: bird.name, species: bird.species}
     else  
       render json: {message: 'Bird not found'}
     end
